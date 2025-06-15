2854870b751187802b931e9db9a5b4b393c968f1
Adel Srour <adelmalys@hotmail.com>
2025-06-15 23:07:35 +0300

    Change ratelimiter config

--- lab3/middlewares/rateLimiter.js
+++ lab3/middlewares/rateLimiter.js
@@ -1,8 +1,9 @@
 const rateLimit = require("express-rate-limit");
+const AppError = require("../util/AppError");
 
 const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  limit: 10,
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Redis, Memcached, etc. See below.
